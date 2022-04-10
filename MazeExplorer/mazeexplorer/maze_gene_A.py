36a7d4f85547f2949359026b40911cdd2e8bc8ed
mondayyy <monday091233@gmail.com>
2022-04-10 16:03:57 -0400

    Recursive function
    Added recursive method.

--- MazeExplorer/mazeexplorer/maze_gene_A.py
+++ MazeExplorer/mazeexplorer/maze_gene_A.py
@@ -25,41 +25,58 @@ class WallNode:
       return [self.x, self.y, self.dx, self.dy, self.height]
 
     def find_child(self, rows, columns, X = False, Y = False, rat = 0):
-      """
-      TODO: Think about the method of X and Y and return. 
-      
-      args: 
+        """
+          TODO:
+              1. Think about the method of X and Y and return.
+              2. Let the method able to go upward or downward. 
+          
+          args: 
+    
+          rows: the rows of the maze. 
+          columns: the columns of the maze. 
+          X and Y: whether the wall point of that direction will be generate
+          rat: the possibility to change the height of the wall. 
+          
+          return: child node. 
+         """
+        
+        self.dx = random.randint(self.x, columns)
+        self.dy = random.randint(self.y, rows)
+        #print("dx, dy: ", self.dx, self.dy)
+        #print(self.show_node())
+        if X != False: 
+            n = WallNode(self.x, self.dy, self, self.height)
+            if random.random() < rat:
+                n.height = random.randint(0, 10)
+        if Y != False: 
+            m = WallNode (self.dx, self.y, self, self.height)
+            if random.random() < rat:
+                m.height = random.randint(0, 10)
+        return n, m
+  
+    def rec_generator(self, maze, rows, columns, X = False, Y = False, rat = 0):
+        """
+        TODO: Try recursive function. 
+        """
+        
+        return 
 
-      rows: the rows of the maze. 
-      columns: the columns of the maze. 
-      X and Y: whether the wall point of that direction will be generate
-      rat: the possibility to change the wall height. 
-      
-      return: child node. 
-      """
-      self.dx = random.randint(self.x, columns)
-      self.dy = random.randint(self.y, rows)
-      #print("dx, dy: ", self.dx, self.dy)
-      #print(self.show_node())
-      if X != False: 
-        n = WallNode(self.x, self.dy, self, self.height)
-        if random.random() < rat:
-          n.height = random.randint(0, 10)
-      if Y != False: 
-        m = WallNode (self.dx, self.y, self, self.height)
-        if random.random() < rat:
-          m.height = random.randint(0, 10)
-      return n, m
-
-
-
+def original_node(rows = 0, columns = 0, h = 1):
+    x = random.randint(0, columns)
+    y = random.randint(0, rows)
+    
+    return WallNode(x, y, None, h)
+        
 
   
 def generate_maze_A (rows=20, columns=20, numOfNodes=20):
     
     """
-    TODO: Count is now counting the nodes of the corner. 
-    need to be change to the number of total walls. 
+    TODO: 
+        1. Count is now counting the nodes of the corner. 
+            need to be change to the number of total walls. 
+        2. Last point should be the original node. 
+        
     """
     
     maze = list()
@@ -77,8 +94,21 @@ def generate_maze_A (rows=20, columns=20, numOfNodes=20):
         maze.append(child2)
         count += 2
         node = child1
-        
     
     return maze
 
 
+
+
+def rec_maze_A(node=None, maze=[],  rows=20, columns=20, numOfNodes=20): 
+    #Start of the process. 
+    if node == None: 
+        node = original_node(rows, columns)
+        
+    #print(node.show_node())
+    maze.append(node.show_node())
+    if len(maze) > numOfNodes: 
+        return maze
+    
+    child1, child2 = node.find_child(rows, columns, X = True, Y = True, rat = 0.1)
+    return rec_maze_A(child1, maze, rows, columns, numOfNodes) and rec_maze_A(child2, maze, rows, columns, numOfNodes)
--- MazeExplorer/mazeexplorer/test_maze_A.py
+++ MazeExplorer/mazeexplorer/test_maze_A.py
@@ -5,12 +5,20 @@ from __future__ import print_function
 
 import os
 import random
-from maze_gene_A import generate_maze_A, WallNode
+from maze_gene_A import generate_maze_A, WallNode, rec_maze_A
 
 import numpy as np
 
-test_maze = generate_maze_A()
-#print(test_maze)
+# test_maze = generate_maze_A()
+# #print(test_maze)
 
-for m in test_maze: 
-   print(m.show_node())
+# for m in test_maze: 
+#     print(m.show_node())
+
+maze = list()
+test_rec_maze = rec_maze_A()
+print(test_rec_maze)
+
+# for m in test_rec_maze: 
+#     print(m.show_node())
+   
